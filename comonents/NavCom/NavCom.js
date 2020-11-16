f41ce25b45ef536139b37a27f4e9edaec4627725
unknown <yinxiaoyong>
2020-11-16 17:48:50 +0800

    p2

---
+++ comonents/NavCom/NavCom.js
@@ -0,0 +1,30 @@
+// comonents/NavCom/NavCom.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:"我是默认的"
+    },
+    content:{
+      type:String,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
